3b05e8013462399859910f3a3736826594e92c66
Alex <clonalejandromcb@gmail.com>
2020-05-29 03:00:46 +0200

    API - Add https support

--- REST/apiForMc/api.js
+++ REST/apiForMc/api.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const path = require('path')
 const fs = require('fs')
+const http = require('http')
+const https = require('https')
 const resourcesPath = path.join(__dirname, './data/')
 const esJson = require('./data/es.json')
 const arenaJson = require('./data/arena.json')
 const server = express()
 const app = {
-    port: 3000
+    port: 3000,
+    ssl: {
+      key: fs.readFileSync('/etc/letsencrypt/live/play.divecrafts.net/privkey.pem', 'utf8'),
+      cert: fs.readFileSync('/etc/letsencrypt/live/play.divecrafts.net/cert.pem', 'utf8')
+    }
 }
 
 
@@ -14,7 +20,7 @@ const app = {
 
 const get = (id, callback, isApi = true) => !isApi ? server.get(id, callback) : server.get(`/api/${id}`, callback)
 const post = (id, callback) => server.post(`/api/${id}`, callback)
-const readResource = file => JSON.parse(fs.readFileSync(`${resourcesPath}/${file}`, 'utf8'))
+const readResource = file => JSON.parse(fs.readFileSync(file, 'utf8'))
 
 //TODO: Make a ratelimit
 server.use(express.urlencoded())
@@ -22,9 +28,13 @@ server.use(express.json())
 
 fs.readdir(resourcesPath, (err, files) => files.map(
   file =>
-    get(file, (req, res) =>
-      res.send(readResource(file))
+    get(file, (req, res) => file.includes('json') ?
+      res.send(readResource(`${resourcesPath}/${file}`)) :
+      res.sendFile(`${resourcesPath}/${file}`)
     )
 ))
 
-server.listen(app.port, () => console.log(`Backend is up listening the port ${app.port} 🎨`))
+const hook = (port) => console.log(`Backend is up listening the port ${port} 🎨`)
+
+https.createServer(app.ssl, server).listen(3003, hook(3003))
+http.createServer(server).listen(app.port, hook(app.port))
