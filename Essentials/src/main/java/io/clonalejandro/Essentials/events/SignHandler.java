28bc2d33542e8a3c1d9ce0af0ac6a7752bc2b54d
Alex <clonalejandromcb@gmail.com>
2020-06-08 03:22:06 +0200

    Essentials - Add permissions to signhandler

--- Essentials/src/main/java/io/clonalejandro/Essentials/events/SignHandler.java
+++ Essentials/src/main/java/io/clonalejandro/Essentials/events/SignHandler.java
@@ -1,6 +1,11 @@
 package io.clonalejandro.Essentials.events;
 
+import io.clonalejandro.DivecraftsCore.api.SServer;
+import io.clonalejandro.DivecraftsCore.api.SUser;
+import io.clonalejandro.DivecraftsCore.cmd.SCmd;
+import io.clonalejandro.DivecraftsCore.utils.Utils;
 import io.clonalejandro.Essentials.Main;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
@@ -25,8 +30,25 @@ public class SignHandler implements Listener {
 
     @EventHandler
     public void onSign(SignChangeEvent event){
-        for(int i = 0; i<4; i++){
+        if (checkPermissions(event.getPlayer(), SCmd.Rank.NEMO)) return;
+
+        for(int i = 0; i < 4; i++){
             event.setLine(i, Main.translate(event.getLine(i)));
         }
+    }
+
+
+    public boolean checkPermissions(Player player, SCmd.Rank rank){
+        final SUser user = SServer.getUser(player.getUniqueId());
+
+        if (user.getUserData().getRank().getRank() < rank.getRank())
+            return sendErrMessage(player);
+
+        return false;
+    }
+
+    private boolean sendErrMessage(Player player){
+        player.sendMessage(Utils.colorize("&c&lServer> &fNo tienes permisos para hacer eso!"));
+        return true;
     }
 }
