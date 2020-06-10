2276322c08edeed3f3925fe72ad5070515721661
Alex <clonalejandromcb@gmail.com>
2020-06-10 03:07:22 +0200

    Essentials - Add head drop on player death

--- Essentials/src/main/java/io/clonalejandro/Essentials/events/PlayerHandlers.java
+++ Essentials/src/main/java/io/clonalejandro/Essentials/events/PlayerHandlers.java
@@ -1,11 +1,16 @@
 package io.clonalejandro.Essentials.events;
 
 import io.clonalejandro.DivecraftsCore.utils.Utils;
+import io.clonalejandro.Essentials.commands.HeadCmd;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Alex
@@ -30,13 +35,32 @@ public class PlayerHandlers implements Listener {
         final Player player = event.getEntity();
         final Entity entity = player.getKiller();
 
-        String msg = event.getDeathMessage() == null ? "" : event.getDeathMessage();
+        String msg = "%player ha sido asesinado por %killer";
 
-        msg = msg.replace(player.getName(), player.getDisplayName());
+        msg = msg.replace("%player", player.getDisplayName());
 
         if (entity != null && entity instanceof Player)
-            msg = msg.replace(entity.getName(), ((Player) entity).getDisplayName());
+            msg = msg.replace("%killer", ((Player) entity).getDisplayName());
+        else if (entity != null) {
+            msg = msg.replace("%killer", entity.getCustomName());
+        }
 
-        event.setDeathMessage(Utils.colorize("&7") + msg);
+        if (hasLucky(1, 1000) && entity instanceof Player){
+            final ItemStack head = HeadCmd.createSkull(
+                    Utils.colorize("&eHead of " + player.getDisplayName()),
+                    player.getName(),
+                    new ArrayList<>()
+            );
+
+            player.getLocation().getWorld().dropItemNaturally(player.getLocation(), head);
+        }
+
+        event.setDeathMessage(Utils.colorize("&7" + msg));
+    }
+
+    public boolean hasLucky(int min, int max){
+        final int secretNumber = 1;
+        final int number = new Random().nextInt((max - min) + 1) - min;
+        return number == secretNumber;
     }
 }
