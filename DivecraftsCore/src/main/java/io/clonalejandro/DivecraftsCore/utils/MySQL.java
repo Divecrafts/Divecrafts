f1be8dea61b387e853f2c8dc116b9a9d70ffbd50
Alex <clonalejandromcb@gmail.com>
2020-06-14 03:01:57 +0200

    Core - Fix core mysql

--- DivecraftsCore/src/main/java/io/clonalejandro/DivecraftsCore/utils/MySQL.java
+++ DivecraftsCore/src/main/java/io/clonalejandro/DivecraftsCore/utils/MySQL.java
@@ -393,7 +393,7 @@ public class MySQL {
             boosters.add(new SBooster(
                     rs.getInt("id"),
                     rs.getInt("multiplier"),
-                    SServer.GameID.values()[rs.getInt("gameId")],
+                    SServer.GameID.values()[rs.getInt("gameId") -1],
                     Timestamp.valueOf(rs.getString("expires")),
                     UUID.fromString(rs.getString("uuid"))
             ));
